40c9038ad2daac44f8fab82ae74465c776c67761
florianthiery <web@florian-thiery.de>
2016-08-12 12:02:27 +0200

    api javadoc

--- api/src/main/java/info/labeling/v1/utils/ConfigProperties.java
+++ api/src/main/java/info/labeling/v1/utils/ConfigProperties.java
@@ -4,30 +4,49 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+/**
+ * CLASS for property file actions
+ *
+ * @author Florian Thiery M.Sc.
+ */
 public class ConfigProperties {
-    
-    private static final Properties prop = new Properties();
+
+	private static final Properties prop = new Properties();
 	private static final String fileName = "config.properties";
-    
-    public static boolean loadpropertyFile(String fileName) throws IOException {
-        InputStream input = null;
-        try {
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            input = classLoader.getResourceAsStream(fileName);
-            prop.load(input);
+
+	/**
+	 * load property file
+	 *
+	 * @param fileName
+	 * @return loaded or not
+	 * @throws IOException
+	 */
+	private static boolean loadpropertyFile(String fileName) throws IOException {
+		InputStream input = null;
+		try {
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			input = classLoader.getResourceAsStream(fileName);
+			prop.load(input);
 			return true;
-        } catch (Exception e) {
+		} catch (Exception e) {
 			throw new IOException(e.toString());
-        } finally {
-            if (input != null) {
-                input.close();
-            }
-        }
-    }
-    
-    public static String getPropertyParam(String param) throws IOException {
-        loadpropertyFile(fileName);
-        return prop.getProperty(param);
-    }
-    
+		} finally {
+			if (input != null) {
+				input.close();
+			}
+		}
+	}
+
+	/**
+	 * get parameter of property file
+	 *
+	 * @param param
+	 * @return param value
+	 * @throws IOException
+	 */
+	public static String getPropertyParam(String param) throws IOException {
+		loadpropertyFile(fileName);
+		return prop.getProperty(param);
+	}
+
 }
