d4dafa122fe896737eab187c7e2a9eb6ea8f25f4
Florian Thiery <web@florian-thiery.de>
2016-09-06 09:33:05 +0200

    api: add servlet for init info

---
+++ api/src/main/java/servlet/Main.java
@@ -0,0 +1,77 @@
+package servlet;
+
+import exceptions.Logging;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Response;
+import org.json.simple.JSONObject;
+import v1.rest.InfoResource;
+import v1.utils.config.ConfigProperties;
+
+public class Main extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            JSONObject outObject = new JSONObject();
+            outObject.put("title", "Labeling System API");
+            JSONObject versionObject = new JSONObject();
+            versionObject.put("1.0", ConfigProperties.getPropertyParam("api") + "/v1");
+            outObject.put("versions", versionObject);
+            outObject.put("wiki", "https://github.com/labelingsystem/server/wiki");
+            // get last build data
+            File file = new File(InfoResource.class.getClassLoader().getResource("config.properties").getFile());
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            outObject.put("last build", sdf.format(file.lastModified()));
+            out.print(outObject);
+        } catch (Exception e) {
+            response.sendError(500, e.toString());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
