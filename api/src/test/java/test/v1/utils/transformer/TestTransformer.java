baab89bf60830d1b3d48909c1f575641bf37e6d2
florianthiery <web@florian-thiery.de>
2016-09-08 14:36:06 +0200

    api: add first test function

---
+++ api/src/test/java/test/v1/utils/transformer/TestTransformer.java
@@ -0,0 +1,16 @@
+package test.v1.utils.transformer;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import v1.utils.transformer.Transformer;
+
+public class TestTransformer {
+	
+	// http://www.tutorialspoint.com/junit/junit_using_assertion.htm
+	
+	@Test
+	public void givenValidFileShouldReturnTrue() throws Exception {
+		assertNotNull(Transformer.vocabularyDifference(null,null));
+	}
+	
+}
