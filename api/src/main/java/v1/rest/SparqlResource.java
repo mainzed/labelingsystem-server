6c4ce65fda3bf5dc821ed4b52db11b96b6c888f7
Florian Thiery <web@florian-thiery.de>
2016-11-21 15:42:45 +0100

    api: fixed bug global variable sparql endpoint

--- api/src/main/java/v1/rest/SparqlResource.java
+++ api/src/main/java/v1/rest/SparqlResource.java
@@ -20,146 +20,167 @@ import javax.ws.rs.core.StreamingOutput;
 @Path("/sparql")
 public class SparqlResource {
 
-    private static String QUERY = "SELECT * WHERE { ?s ?p ?o } LIMIT 10";
-    private static String FORMAT = "xml";
-    private static String CONTENT_TYPE = "application/sparql-results+xml";
+	// https://www.w3.org/TR/sparql11-protocol/
+	// http://de.slideshare.net/olafhartig/querying-linked-data-with-sparql
+	@GET
+	@Produces({"application/json;charset=UTF-8", "application/xml;charset=UTF-8", "application/sparql-results+json;charset=UTF-8", "application/sparql-results+xml;charset=UTF-8", "text/plain;charset=UTF-8", "text/csv;charset=UTF-8", "text/tsv;charset=UTF-8"})
+	public Response getSPARQLresultsGET(@HeaderParam("Accept") String acceptHeader, @QueryParam("query") String query, @QueryParam("out") String out) {
+		String QUERY = "SELECT * WHERE { ?s ?p ?o } LIMIT 10";
+		String FORMAT = "xml";
+		String CONTENT_TYPE = "application/sparql-results+xml";
+		try {
+			// parse query
+			if (query != null) {
+				QUERY = query;
+				QUERY = URLDecoder.decode(QUERY, "UTF-8");
+			} else {
+				throw new NullPointerException();
+			}
+			// set format if requested by var
+			if (out != null) {
+				if (out.equals("json")) {
+					FORMAT = "json";
+					CONTENT_TYPE = "application/sparql-results+json;charset=UTF-8";
+				} else if (out.equals("xml")) {
+					FORMAT = "xml";
+					CONTENT_TYPE = "application/sparql-results+xml;charset=UTF-8";
+				} else if (out.equals("csv")) {
+					FORMAT = "csv";
+					CONTENT_TYPE = "text/plain;charset=UTF-8";
+				} else if (out.equals("tsv")) {
+					FORMAT = "tsv";
+					CONTENT_TYPE = "text/plain;charset=UTF-8";
+				} else {
+					FORMAT = "xml";
+					CONTENT_TYPE = "application/sparql-results+xml;charset=UTF-8";
+				}
+			} else // set format if requested by accept header
+			{
+				if (acceptHeader != null) {
+					if (acceptHeader.contains("application/sparql-results+xml")) {
+						FORMAT = "xml";
+						CONTENT_TYPE = "application/sparql-results+xml;charset=UTF-8";
+					} else if (acceptHeader.contains("application/sparql-results+json")) {
+						FORMAT = "json";
+						CONTENT_TYPE = "application/sparql-results+json;charset=UTF-8";
+					} else if (acceptHeader.contains("text/csv")) {
+						FORMAT = "csv";
+						CONTENT_TYPE = "text/plain;charset=UTF-8";
+					} else if (acceptHeader.contains("text/tsv")) {
+						FORMAT = "tsv";
+						CONTENT_TYPE = "text/plain;charset=UTF-8";
+					} else if (acceptHeader.contains("application/xml")) {
+						FORMAT = "xml";
+						CONTENT_TYPE = "application/xml;charset=UTF-8";
+					} else if (acceptHeader.contains("application/json")) {
+						FORMAT = "json";
+						CONTENT_TYPE = "application/json;charset=UTF-8";
+					} else {
+						FORMAT = "xml";
+						CONTENT_TYPE = "application/sparql-results+xml";
+					}
+				}
+			}
+			// set outputstream
+			final String FORMAT_FINAL = FORMAT;
+			final String QUERY_FINAL = QUERY;
+			StreamingOutput stream;
+			stream = new StreamingOutput() {
+				@Override
+				public void write(OutputStream output) throws IOException, WebApplicationException {
+					try {
+						RDF4J_20.SPARQLqueryOutputFileOS(ConfigProperties.getPropertyParam("repository"),
+								ConfigProperties.getPropertyParam("ts_server"), QUERY_FINAL, FORMAT_FINAL, output);
+					} catch (Exception e) {
+					}
+				}
+			};
+			// get output
+			return Response.ok(stream).header("Content-Type", CONTENT_TYPE).build();
+		} catch (Exception e) {
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Logging.getMessageJSON(e, "v1.rest.SparqlResource"))
+					.header("Content-Type", "application/json;charset=UTF-8").build();
+		}
+	}
 
-    // https://www.w3.org/TR/sparql11-protocol/
-    // http://de.slideshare.net/olafhartig/querying-linked-data-with-sparql
-    @GET
-    @Produces({"application/json;charset=UTF-8", "application/xml;charset=UTF-8", "application/sparql-results+json;charset=UTF-8", "application/sparql-results+xml;charset=UTF-8", "text/plain;charset=UTF-8", "text/csv;charset=UTF-8", "text/tsv;charset=UTF-8"})
-    public Response getSPARQLresultsGET(@HeaderParam("Accept") String acceptHeader, @QueryParam("query") String query, @QueryParam("out") String out) {
-        try {
-            // parse query
-            if (query != null) {
-                QUERY = query;
-                QUERY = URLDecoder.decode(QUERY, "UTF-8");
-            } else {
-                throw new NullPointerException();
-            }
-            // set format if requested by var
-            if (out != null) {
-                if (out.equals("json")) {
-                    FORMAT = "json";
-                    CONTENT_TYPE = "application/sparql-results+json;charset=UTF-8";
-                } else if (out.equals("xml")) {
-                    FORMAT = "xml";
-                    CONTENT_TYPE = "application/sparql-results+xml;charset=UTF-8";
-                } else if (out.equals("csv")) {
-                    FORMAT = "csv";
-                    CONTENT_TYPE = "text/plain;charset=UTF-8";
-                } else if (out.equals("tsv")) {
-                    FORMAT = "tsv";
-                    CONTENT_TYPE = "text/plain;charset=UTF-8";
-                } else {
-                    FORMAT = "xml";
-                    CONTENT_TYPE = "application/sparql-results+xml;charset=UTF-8";
-                }
-            } else // set format if requested by accept header
-            if (acceptHeader != null) {
-                if (acceptHeader.contains("application/sparql-results+xml")) {
-                    FORMAT = "xml";
-                    CONTENT_TYPE = "application/sparql-results+xml;charset=UTF-8";
-                } else if (acceptHeader.contains("application/sparql-results+json")) {
-                    FORMAT = "json";
-                    CONTENT_TYPE = "application/sparql-results+json;charset=UTF-8";
-                } else if (acceptHeader.contains("text/csv")) {
-                    FORMAT = "csv";
-                    CONTENT_TYPE = "text/plain;charset=UTF-8";
-                } else if (acceptHeader.contains("text/tsv")) {
-                    FORMAT = "tsv";
-                    CONTENT_TYPE = "text/plain;charset=UTF-8";
-                } else if (acceptHeader.contains("application/xml")) {
-                    FORMAT = "xml";
-                    CONTENT_TYPE = "application/xml;charset=UTF-8";
-                } else if (acceptHeader.contains("application/json")) {
-                    FORMAT = "json";
-                    CONTENT_TYPE = "application/json;charset=UTF-8";
-                } else {
-                    FORMAT = "xml";
-                    CONTENT_TYPE = "application/sparql-results+xml";
-                }
-            }
-            // get output
-            return Response.ok(new FeedReturnStreamingOutput()).header("Content-Type", CONTENT_TYPE).build();
-        } catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Logging.getMessageJSON(e, "v1.rest.SparqlResource"))
-                    .header("Content-Type", "application/json;charset=UTF-8").build();
-        }
-    }
-	
 	@POST
-    @Produces({"application/json;charset=UTF-8", "application/xml;charset=UTF-8", "application/sparql-results+json;charset=UTF-8", "application/sparql-results+xml;charset=UTF-8", "text/plain;charset=UTF-8", "text/csv;charset=UTF-8", "text/tsv;charset=UTF-8"})
-    public Response getSPARQLresultsGET2(@HeaderParam("Accept") String acceptHeader, @FormParam("query") String query, @FormParam("out") String out) {
-        try {
-            // parse query
-            if (query != null) {
-                QUERY = query;
-                QUERY = URLDecoder.decode(QUERY, "UTF-8");
-            } else {
-                throw new NullPointerException();
-            }
-            // set format if requested by var
-            if (out != null) {
-                if (out.equals("json")) {
-                    FORMAT = "json";
-                    CONTENT_TYPE = "application/sparql-results+json;charset=UTF-8";
-                } else if (out.equals("xml")) {
-                    FORMAT = "xml";
-                    CONTENT_TYPE = "application/sparql-results+xml;charset=UTF-8";
-                } else if (out.equals("csv")) {
-                    FORMAT = "csv";
-                    CONTENT_TYPE = "text/plain;charset=UTF-8";
-                } else if (out.equals("tsv")) {
-                    FORMAT = "tsv";
-                    CONTENT_TYPE = "text/plain;charset=UTF-8";
-                } else {
-                    FORMAT = "xml";
-                    CONTENT_TYPE = "application/sparql-results+xml;charset=UTF-8";
-                }
-            } else // set format if requested by accept header
-            if (acceptHeader != null) {
-                if (acceptHeader.contains("application/sparql-results+xml")) {
-                    FORMAT = "xml";
-                    CONTENT_TYPE = "application/sparql-results+xml;charset=UTF-8";
-                } else if (acceptHeader.contains("application/sparql-results+json")) {
-                    FORMAT = "json";
-                    CONTENT_TYPE = "application/sparql-results+json;charset=UTF-8";
-                } else if (acceptHeader.contains("text/csv")) {
-                    FORMAT = "csv";
-                    CONTENT_TYPE = "text/plain;charset=UTF-8";
-                } else if (acceptHeader.contains("text/tsv")) {
-                    FORMAT = "tsv";
-                    CONTENT_TYPE = "text/plain;charset=UTF-8";
-                } else if (acceptHeader.contains("application/xml")) {
-                    FORMAT = "xml";
-                    CONTENT_TYPE = "application/xml;charset=UTF-8";
-                } else if (acceptHeader.contains("application/json")) {
-                    FORMAT = "json";
-                    CONTENT_TYPE = "application/json;charset=UTF-8";
-                } else {
-                    FORMAT = "xml";
-                    CONTENT_TYPE = "application/sparql-results+xml";
-                }
-            }
-            // get output
-            return Response.ok(new FeedReturnStreamingOutput()).header("Content-Type", CONTENT_TYPE).build();
-        } catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Logging.getMessageJSON(e, "v1.rest.SparqlResource"))
-                    .header("Content-Type", "application/json;charset=UTF-8").build();
-        }
-    }
-
-    private static class FeedReturnStreamingOutput implements StreamingOutput {
-
-        @Override
-        public void write(OutputStream output) throws IOException, WebApplicationException {
-            try {
-                RDF4J_20.SPARQLqueryOutputFileOS(ConfigProperties.getPropertyParam("repository"),
-                        ConfigProperties.getPropertyParam("ts_server"), QUERY, FORMAT, output);
-            } catch (Exception e) {
-            }
-        }
-    }
-
+	@Produces({"application/json;charset=UTF-8", "application/xml;charset=UTF-8", "application/sparql-results+json;charset=UTF-8", "application/sparql-results+xml;charset=UTF-8", "text/plain;charset=UTF-8", "text/csv;charset=UTF-8", "text/tsv;charset=UTF-8"})
+	public Response getSPARQLresultsGET2(@HeaderParam("Accept") String acceptHeader, @FormParam("query") String query, @FormParam("out") String out) {
+		String QUERY = "SELECT * WHERE { ?s ?p ?o } LIMIT 10";
+		String FORMAT = "xml";
+		String CONTENT_TYPE = "application/sparql-results+xml";
+		try {
+			// parse query
+			if (query != null) {
+				QUERY = query;
+				QUERY = URLDecoder.decode(QUERY, "UTF-8");
+			} else {
+				throw new NullPointerException();
+			}
+			// set format if requested by var
+			if (out != null) {
+				if (out.equals("json")) {
+					FORMAT = "json";
+					CONTENT_TYPE = "application/sparql-results+json;charset=UTF-8";
+				} else if (out.equals("xml")) {
+					FORMAT = "xml";
+					CONTENT_TYPE = "application/sparql-results+xml;charset=UTF-8";
+				} else if (out.equals("csv")) {
+					FORMAT = "csv";
+					CONTENT_TYPE = "text/plain;charset=UTF-8";
+				} else if (out.equals("tsv")) {
+					FORMAT = "tsv";
+					CONTENT_TYPE = "text/plain;charset=UTF-8";
+				} else {
+					FORMAT = "xml";
+					CONTENT_TYPE = "application/sparql-results+xml;charset=UTF-8";
+				}
+			} else // set format if requested by accept header
+			{
+				if (acceptHeader != null) {
+					if (acceptHeader.contains("application/sparql-results+xml")) {
+						FORMAT = "xml";
+						CONTENT_TYPE = "application/sparql-results+xml;charset=UTF-8";
+					} else if (acceptHeader.contains("application/sparql-results+json")) {
+						FORMAT = "json";
+						CONTENT_TYPE = "application/sparql-results+json;charset=UTF-8";
+					} else if (acceptHeader.contains("text/csv")) {
+						FORMAT = "csv";
+						CONTENT_TYPE = "text/plain;charset=UTF-8";
+					} else if (acceptHeader.contains("text/tsv")) {
+						FORMAT = "tsv";
+						CONTENT_TYPE = "text/plain;charset=UTF-8";
+					} else if (acceptHeader.contains("application/xml")) {
+						FORMAT = "xml";
+						CONTENT_TYPE = "application/xml;charset=UTF-8";
+					} else if (acceptHeader.contains("application/json")) {
+						FORMAT = "json";
+						CONTENT_TYPE = "application/json;charset=UTF-8";
+					} else {
+						FORMAT = "xml";
+						CONTENT_TYPE = "application/sparql-results+xml";
+					}
+				}
+			}
+			// set outputstream
+			final String FORMAT_FINAL = FORMAT;
+			final String QUERY_FINAL = QUERY;
+			StreamingOutput stream;
+			stream = new StreamingOutput() {
+				@Override
+				public void write(OutputStream output) throws IOException, WebApplicationException {
+					try {
+						RDF4J_20.SPARQLqueryOutputFileOS(ConfigProperties.getPropertyParam("repository"),
+								ConfigProperties.getPropertyParam("ts_server"), QUERY_FINAL, FORMAT_FINAL, output);
+					} catch (Exception e) {
+					}
+				}
+			};
+			// get output
+			return Response.ok(stream).header("Content-Type", CONTENT_TYPE).build();
+		} catch (Exception e) {
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Logging.getMessageJSON(e, "v1.rest.SparqlResource"))
+					.header("Content-Type", "application/json;charset=UTF-8").build();
+		}
+	}
 }
