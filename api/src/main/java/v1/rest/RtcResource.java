66b2e0a32b992a00d7c6d3f5ca73f17d4be68c02
Florian Thiery <web@florian-thiery.de>
2018-02-23 10:49:39 +0100

    add RtcResource extends RetcatREST

---
+++ api/src/main/java/v1/rest/RtcResource.java
@@ -0,0 +1,27 @@
+package v1.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import link.labeling.retcat.utils.RetcatREST;
+import org.json.simple.JSONObject;
+
+@Path("/rtc")
+public class RtcResource extends RetcatREST {
+
+    public RtcResource() {
+        super();
+    }
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+    public Response getInfo() {
+        JSONObject out = new JSONObject();
+        out.put("CoffeePot", "https://tools.ietf.org/html/rfc2324");
+        return Response.status(418).entity(out).build();
+    }
+
+}
