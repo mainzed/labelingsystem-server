37979f1fea8256d0e3f3d4d4c66dadb12d991c63
Florian Thiery <web@florian-thiery.de>
2018-02-26 11:30:11 +0100

    chnage retcat lib package name

--- api/src/main/java/v1/rest/RtcResource.java
+++ api/src/main/java/v1/rest/RtcResource.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import link.labeling.retcat.utils.RetcatREST;
+import link.labeling.retcat.rest.RetcatREST;
 import org.json.simple.JSONObject;
 
 @Path("/rtc")
